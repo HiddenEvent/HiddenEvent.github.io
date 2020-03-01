3de95c6de92fcdc844a268733bc0dd98acd0afc2
RichardKim <hiddeneventor@gmail.com>
2020-03-01 23:43:42 +0900

    캡션 커스텀마이징

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 // turn img alt into caption
 $('.page__content > p > img[alt]').replaceWith(function () {
     return '<figure>'
+        + '<figcaption class="caption" style="font-weight: 600;">▼ ' + $(this).attr('alt') + '</figcaption>'
         + '<a href="' + $(this).attr('src') + '" class="mg-link">'
         + '<img src="' + $(this).attr('src') + '"/></a>'
-        + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
         + '</figure>';
 });
 // and connect magnific popup image viewer
