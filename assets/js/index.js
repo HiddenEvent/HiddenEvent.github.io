2fcef7c7821691574d6f04b03ed9968f779794ea
RichardKim <hiddeneventor@gmail.com>
2020-03-01 23:49:59 +0900

    캡션 설정 변경

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 // turn img alt into caption
 $('.page__content > p > img[alt]').replaceWith(function () {
     return '<figure>'
-        + '<figcaption class="caption" style="font-weight: 600;">▼ ' + $(this).attr('alt') + '</figcaption>'
+        + '<figcaption class="caption" style="font-weight: 600;width:100%">▼ ' + $(this).attr('alt') + '</figcaption>'
         + '<a href="' + $(this).attr('src') + '" class="mg-link">'
         + '<img src="' + $(this).attr('src') + '"/></a>'
         + '</figure>';
